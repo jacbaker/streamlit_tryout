c0cd6129540d2583705c7489daa29ce3b561116c
jake <jacbaker@gmail.com>
2025-07-13 05:52:26 +0100

    put on one grid

--- tax_app.py
+++ tax_app.py
@@ -2,12 +2,15 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
+st.sidebar.subheader('Adjust income and exchange rate using the sliders')
 income = st.sidebar.slider('income (aussie dollars)', 10000, 400000, 100000)
 exchange_rate = st.sidebar.slider('exchange rate £ to $', 1.5, 2.5, 2.05)
 
 uk_equivalent = income / exchange_rate
 
+st.sidebar.write(f'income: ${income:,.2f}')
 st.sidebar.write(f'uk equivalent: £{uk_equivalent:,.2f}')
 st.sidebar.write(f'at exchange rate £1 = ${exchange_rate:.2f}')
 
@@ -47,32 +50,27 @@ aud_equivalent_uktax = uktax * exchange_rate
 
 st.subheader(f'UK tax: £{uktax:,.2f} which is equivalent to ${aud_equivalent_uktax:,.2f}')
 
-x_axis_series = [i for i in range(0, 400000, 10000)]
-aussie_tax_series = map(calc_aussie_tax, x_axis_series)
-uk_tax_series = map(calc_uk_tax, map(lambda inc: inc / exchange_rate, x_axis_series))
+x_axis_series = [i for i in range(0, 410000, 10000)]
+aussie_tax_series = list(map(calc_aussie_tax, x_axis_series))
+uk_tax_series = list(map(calc_uk_tax, map(lambda inc: inc / exchange_rate, x_axis_series)))
 
-comparison_data_aussie = pd.DataFrame({
-    '$ tax': aussie_tax_series,
-    'income': x_axis_series
-})
-
-fig1 = px.scatter(comparison_data_aussie, x='income', y='$ tax')
-fig1.add_trace(
-    go.Scatter(x=[income], y=[austax], mode='markers', marker_symbol='star', marker_size=15, showlegend=False)
+fig = make_subplots(rows=1, cols=1, shared_yaxes=True)
+fig.update_yaxes(title_text="Tax in $ for Aussie tax and £ for UK tax", range=[0, 170000], dtick=20000)
+fig.update_xaxes(title_text="Income", dtick=50000)
+fig.add_trace(
+    go.Scatter(x=x_axis_series, y=uk_tax_series, name="UK tax"),
+    row=1, col=1
 )
-
-comparison_data_uk = pd.DataFrame({
-    '£ tax': uk_tax_series,
-    'income': x_axis_series
-})
-
-fig2 = px.scatter(comparison_data_uk, x='income', y='£ tax')
-fig2.add_trace(
+fig.add_trace(
+    go.Scatter(x=x_axis_series, y=aussie_tax_series, name="Aussie tax"),
+    row=1, col=1
+)
+fig.add_trace(
     go.Scatter(x=[income], y=[uktax], mode='markers', marker_symbol='star', marker_size=15, showlegend=False)
 )
-
-col1, col2 = st.columns(2)
-col1.subheader('Aussie tax')
-col2.subheader('UK tax')
-col1.plotly_chart(fig1, use_container_width=True)
-col2.plotly_chart(fig2, use_container_width=True)
+fig.add_trace(
+    go.Scatter(x=[income], y=[austax], mode='markers', marker_symbol='star', marker_size=15, showlegend=False)
+)
+fig.update_layout(title_text="Aussie tax vs UK tax")
+# fig.show()
+st.plotly_chart(fig, use_container_width=True)
